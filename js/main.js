cd431750afd4adb7330b4d85bd5593293ebdd16e
Micah Jackson <thehazelwize@gmail.com>
2022-04-13 21:23:02 -0400

    Allowed multiple search results to display

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
 document.querySelector('button').addEventListener('click', getName)
-function capFirst(str) {
-   
-   
-   
-}
+let resultSec = document.querySelector('#result')
+document.querySelector('input').addEventListener('keyup', function(event) {
+   if(event.code === "Enter"){
+      document.querySelector('button').click()
+   }
+})
+
+
 
 function getName() {
-   fetch("https://www.amiiboapi.com/api/amiibo")
+   document.querySelector('#result').innerHTML = ''
+   let name = document.querySelector('input').value.toLowerCase()
+   fetch(`https://www.amiiboapi.com/api/amiibo/?name=${name}`)
    .then(res => res.json())
    .then(data =>{
-      console.log(data.amiibo);
-      let name = document.querySelector('input').value.toLowerCase()
-      let nav = data.amiibo.findIndex(e => e.character.toLowerCase() == name);
-      console.log(nav);
-      console.log(name)
-      document.querySelector('h2').innerText = data.amiibo[nav].name
-      document.querySelector('img').src = data.amiibo[nav].image
-      document.querySelector('h3').innerText = `Series: ${data.amiibo[nav].gameSeries}`
+      console.log(data);
+      function createCard(e) {
+         let div = document.createElement('div')
+         let img = document.createElement('img')
+         let title = document.createElement('h3')
+         let series = document.createElement('h5')
+         resultSec.appendChild(div)
+         div.appendChild(img)
+         div.appendChild(title)
+         div.appendChild(series)
+         div.querySelector(`img`).src = e.image
+         div.querySelector(`h3`).innerText = `Name: ${e.name}`
+         div.querySelector('h5').innerText = `Series: ${e.amiiboSeries}`
+      }
+      data.amiibo.forEach((e) => {
+        createCard(e);
+
+      })
    })
    .catch(err =>{
       console.log(`error ${err}`)
